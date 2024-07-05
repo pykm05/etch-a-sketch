8b4e55bebd3b8da4a9d7445e04bb672478687243
Aaron Pang <aaroncwpang@gmail.com>
2024-07-05 15:25:37 -0700

    added resizability to canvas dimensions up to 100

--- script.js
+++ script.js
@@ -3,18 +3,43 @@ const container = document.querySelector('.container');
 
 let count = 16;
 
-for (let i = 0; i < count; i++) {
-  for (let j = 0; j < count; j++) {
-    let tile = document.createElement('div');
-    tile.classList.add('tile');
-    tile.style.width = 960 / count + "px";
-    tile.style.height = 960 / count + "px";
-    tile.addEventListener('mouseover', () => {
-      tile.style.backgroundColor = "red";
-    })
-    container.appendChild(tile);
+let draw = (count) => {
+  for (let i = 0; i < count; i++) {
+    for (let j = 0; j < count; j++) {
+      let tile = document.createElement('div');
+      tile.classList.add('tile');
+      tile.style.width = 960 / count + 'px';
+      tile.style.height = 960 / count + 'px';
+      tile.style.backgroundColor = 'white';
+      tile.addEventListener('mouseover', () => {
+        tile.style.backgroundColor = 'red';
+      })
+     container.appendChild(tile);
+    }
   }
 }
 
+let redraw = (count) => {
+  if (count >= 100) {
+    count = 100;
+  }
+  else if (count <= 0) {
+    count = 16;
+  }
+  while(container.firstChild) container.removeChild(container.firstChild);
+  draw(count);
+}
+
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+
+button.addEventListener('click', () => {
+  count = input.value;
+  redraw(count);
+  console.log(count);
+})
+
+input.focus();
+draw(count);
 body.appendChild(container);
 
